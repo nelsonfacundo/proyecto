e6f50dda4b501d7d60d62a7e363b9c902ed38c9a
Agustin Camarero <aguscamarero@hotmail.com>
2020-01-19 21:44:02 -0300

    agrego faq y css

--- faq.php
+++ faq.php
@@ -1,96 +1,74 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
+<html lang="en" dir="ltr">
+  <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>F.A.Q.</title>
     <link rel="stylesheet" href="css/faq.css">
+    <link href="https://fonts.googleapis.com/css?family=Fredoka+One&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans|Raleway|Work+Sans&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/86f3787a46.js" crossorigin="anonymous"></script>
-</head>
-<body>
+    <title></title>
+  </head>
+  <body>
     <div class="container">
       <div id="header">
-        <div class="main-header">
-            <img src="img/logo.jpg" alt="logo">
-            <input type="checkbox" id="menu-bar">
-             <label class="fas fa-bars" for="menu-bar"></label>
-             <nav class="menu">
-                <ul>
-                       <li><a href="index.php">inicio</a></li>
-                        <li><a href="ranking.php">ranking</a></li>
-                        <li><a href="perfil.php">perfil</a></li>
-                        <li><a href="faq.php">Faq</a></li>
-                        <li><a href="ABM.php">ABM</a></li>
-                        <li><a href="contacto.php">contactanos</a></li>
-                        <li><a href="register.php">registrate</a></li>
-                        <li><a href="login.php">iniciar sesion</a></li>
-                </ul>
-             </nav>
-        </div>
-    </div>
-     <section>
-         <div class="fondo">
-            <ul class="lista">
-                <!-- Pregunta 1 -->
-                <li class="pregunta" data-resp_id="resp_1">
-                  <span>¿Cómo funciona el juego?</span>
-                  <i class="ico_resp fas fa-angle-down"></i>
-                </li>
-                <li class="respuesta" id="resp_1">
-                  <span id="sp_resp_1">Todvia no sabemos.</span>
-                </li>
+          <div class="main-header">
+              <img src="img/logo.jpg" alt="logo">
+              <input type="checkbox" id="menu-bar">
+               <label class="fas fa-bars" for="menu-bar"></label>
+               <nav class="menu">
+                  <ul>
+                      <li><a href="index.html">inicio</a></li>
+                      <li><a href="ranking.html">ranking</a></li>
+                      <li><a href="perfil.html">perfil</a></li>
+                      <li><a href="faq">Faq</a></li>
+                      <li><a href="ABM.html">ABM</a></li>
+                      <li><a href="contacto.html">contactanos</a></li>
+                      <li><a href="register.html">registrate</a></li>
+                      <li><a href="login.html">iniciar sesion</a></li>
+                  </ul>
+               </nav>
+          </div>
+      </div>
+      <div class="contenido">
+        <section id="content-all">
+          <section id="content-header">
+              <h2>Preguntas frecuentes</h2>
+          </section>
+            <div id="cont">
               
-                <!-- Pregunta 2 -->
-                <li class="pregunta" data-resp_id="resp_2">
-                  <span>¿Cómo funciona el ranking?</span>
-                  <i class="ico_resp fas fa-angle-down"></i>
-                </li>
-                <li class="respuesta" id="resp_2">
-                  <span id="sp_resp_2">De nuevo, tampoco lo sabemos.</span>
-                </li>
-
-                 <!-- Pregunta 3 -->
-                 <li class="pregunta" data-resp_id="resp_3">
-                    <span>¿Qué clase de juego es?</span>
-                    <i class="ico_resp fas fa-angle-down"></i>
-                  </li>
-                  <li class="respuesta" id="resp_3">
-                    <span id="sp_resp_3">Cuando lo terminemos, te avisamos.</span>
-                  </li>
-
-                   <!-- Pregunta 4 -->
-                <li class="pregunta" data-resp_id="resp_4">
-                    <span>¿Cuando estará listo?</span>
-                    <i class="ico_resp fas fa-angle-down"></i>
-                  </li>
-                  <li class="respuesta" id="resp_4">
-                    <span id="sp_resp_4">En un futuro cercano.</span>
-                  </li>
+                  <h3>Menu:</h3>
+                    <ul>
+                        <li><a href="#">En que consiste el juego?</a></li>
+                        <li class="respuesta">"Es un juego de preguntas que provara tu ingenio y conocimiento sobre cultura general."</li>
+                        <li><a href="#">Como se suman puntos?</a></li>
+                        <li class="respuesta">"Contestando bien las respuestas"</liclass="respuesta">
+                        <li><a href="#">Como editar mi foto de perfil?</a></li>
+                        <li class="respuesta">"Ir a perfil y seleccionar cambiar foto."</liclass="respuesta">
+                        <li><a href="#">-Como crear usuario?</a></li>
+                        <li class="respuesta">"Ve a la pagina de <a href="#">Inicio.php</a> y selecciona crear usuario"</liclass="respuesta">
+                        <li><a href="#">Como llegar al primer puesto?</a></li>
+                        <li class="respuesta">"Respondiendo la mayor cantidad de preguntas correctas!"</liclass="respuesta">
+                    </ul>
+                </div>    
+        </section>
+      </div>
+      <div id="footer">
+          <img src="img/logo.jpg">
+            <div class="block-footer">
+              <ul>
+                 <li><a href="#">Home</a></li>
+                 <li><a href="#">Quienes</a></li>
+                 <li><a href="#">Servicios</a></li>
+                 <li><a href="#">Sucursales</a></li>
+                 <li><a href="contacto.html">Contacto</a></li>
+                 <li><a href="#">Terminos</a></li>
+                 <li><a href="#">Privacidad</a></li>
+                 <li><a href="#">Cookies</a></li>
               </ul>
-         </div>
-     </section>   
-     <div id="footer">
-      <img src="img/logo.jpg">
-        <div class="block-footer">
-          <ul>
-                <li><a href="index.php">Home</a></li>
-                <li><a href="login.php">Iniciá sesión</a></li>
-                <li><a href="ranking.php">Ranking</a></li>
-                <li><a href="perfil.php">Perfil</a></li>
-                <li><a href="contacto.php">Contacto</a></li>
-                <li><a href="ABM.php">ABM</a></li>
-                <li><a href="faq.php">Preguntas frecuentes</a></li>
-          </ul>
-        </div>
-  </div>
-</div>
-
-       <!-- JQuery -->
-  <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
-  <!-- Script para controlar la lista de preguntas -->
-  <script src="script.js"></script>
-  <!-- FontAwesome -->
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
-</body>
+            </div>
+      </div>
+    </div>
+  </body>
 </html>
